9930676b1d6d9990ece5be4f8cdcaf537d72b537
ChrisMagnuson <hourglasssand@gmail.com>
2017-09-22 14:17:18 -0400

    Updated Edit-StringToPSCustomObjectTemplate, Get-StringToPSCustomObjectTemplatePath, add Invoke-StringToPSCustomObjectTemplate

--- StringTemplateToPSCustomObject.psm1
+++ StringTemplateToPSCustomObject.psm1
@@ -5,8 +5,8 @@
         $ModuleName,
         [ValidateSet("FlashExtract","Regex")]$TemplateType = "FlashExtract"
     )
-    $ModulePath = (Get-Module -ListAvailable $ModuleName).ModuleBase
-    $String | Out-File "$ModulePath\Templates\$FunctionName.$($TemplateType).Template"
+    $Path = Get-StringToPSCustomObjectTemplatePath -FunctionName $FunctionName -ModuleName $ModuleName -TemplateType $TemplateType        
+    $String | Out-File $Path
 }
 
 function Edit-StringToPSCustomObjectTemplate {
@@ -15,7 +15,35 @@ function Edit-StringToPSCustomObjectTemplate {
         $ModuleName,
         [ValidateSet("FlashExtract","Regex")]$TemplateType = "FlashExtract"
     )
+    $Path = Get-StringToPSCustomObjectTemplatePath -FunctionName $FunctionName -ModuleName $ModuleName -TemplateType $TemplateType    
+    Invoke-Item $Path
+}
+
+function Invoke-StringToPSCustomObjectTemplate {
+    param(
+        $String,
+        $FunctionName,
+        $ModuleName,
+        [ValidateSet("FlashExtract","Regex")]$TemplateType = "FlashExtract"
+    )
+    $Path = Get-StringToPSCustomObjectTemplatePath -FunctionName $FunctionName -ModuleName $ModuleName -TemplateType $TemplateType
+
+    if ($TemplateType -eq "FlashExtract") {
+        $String | ConvertFrom-String -TemplateFile $Path
+    } elseif ($TemplateType -eq "Regex") {
+        $String | ConvertFrom-StringUsingRegexCaptureGroup -TemplateFile $Path
+    }
+}
+
+function Get-StringToPSCustomObjectTemplatePath {
+    param(
+        $FunctionName,
+        $ModuleName,
+        [ValidateSet("FlashExtract","Regex")]$TemplateType = "FlashExtract"
+    )
     $ModulePath = (Get-Module -ListAvailable $ModuleName).ModuleBase
-    Invoke-Item "$ModulePath\Templates\$FunctionName.$($TemplateType).Template"
+    "$ModulePath\Templates\$FunctionName.$($TemplateType).Template"
+}
+
 }
 
