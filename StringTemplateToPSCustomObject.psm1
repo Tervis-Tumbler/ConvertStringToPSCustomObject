68a32bf3f5c756f6843cd76ee7bac94375902a1f
ChrisMagnuson <hourglasssand@gmail.com>
2017-09-22 09:55:20 -0400

    Initial commit

---
+++ StringTemplateToPSCustomObject.psm1
@@ -0,0 +1,21 @@
+﻿function New-StringToPSCustomObjectTemplate {
+    param(
+        $String,
+        $FunctionName,
+        $ModuleName,
+        [ValidateSet("FlashExtract","Regex")]$TemplateType = "FlashExtract"
+    )
+    $ModulePath = (Get-Module -ListAvailable $ModuleName).ModuleBase
+    $String | Out-File "$ModulePath\Templates\$FunctionName.$($TemplateType).Template"
+}
+
+function Edit-StringToPSCustomObjectTemplate {
+    param(
+        $FunctionName,
+        $ModuleName,
+        [ValidateSet("FlashExtract","Regex")]$TemplateType = "FlashExtract"
+    )
+    $ModulePath = (Get-Module -ListAvailable $ModuleName).ModuleBase
+    Invoke-Item "$ModulePath\Templates\$FunctionName.$($TemplateType).Template"
+}
+
